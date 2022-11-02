8131f62da63cb54fcde9928e634a5db73715bfa8
Adnan Sardi <104760218+adnan-sardi@users.noreply.github.com>
2022-11-02 18:24:57 +0100

    Add files via upload

---
+++ data_handler.hpp
@@ -0,0 +1,19 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <fstream> 
+#include "stdint.h"                 //For the int 8 data type
+#include "data.hpp" 
+#include <vector>                   //For dinamic array
+#include <string> 
+#include <map>                      /*Maps are associative containers that 
+                                      store elements in a mapped fashion*/
+#include <unordered_set>
+
+class data_handler
+{
+
+
+};
+
+#endif
